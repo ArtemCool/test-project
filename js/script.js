495702378595b8543cddc004f91780ccf589bcb7
Artem <for.the.fallen.man@gmail.com>
2022-03-19 17:54:05 +0300

    try another ways

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const personalMovieDB = {
 //personalMovieDB.movies [a] = b;
 //personalMovieDB.movies [c] = d;
 
-for (let i =0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
