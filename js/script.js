f99fdaf23fd2e815f387ea922c510634ea57e162
Artem <for.the.fallen.man@gmail.com>
2022-03-20 09:48:35 +0300

    remake with function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -18,7 +28,10 @@ const personalMovieDB = {
 //personalMovieDB.movies [a] = b;
 //personalMovieDB.movies [c] = d;
 
-for (let i = 0; i < 2; i++) {
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
@@ -32,8 +45,14 @@ for (let i = 0; i < 2; i++) {
 
     personalMovieDB.movies[a] = b;
 }
+}
 
-if (personalMovieDB.count < 10) {
+rememberMyFilms();
+
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
     console.log('Просмотренно слишком мало фильмов!');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Вы классический зритель!');
@@ -43,5 +62,24 @@ if (personalMovieDB.count < 10) {
         console.log('Произошла ошибка!');
     
 }
+}
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt (`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+
